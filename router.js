4a6204840a64a67927a5562849186bdb0d62609e
aretw0 <aretw0@hotmail.com>
2016-12-09 17:51:37 -0300

    Fim do dia
    É tudo isso por hoje pessoal!!

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-function route(handle,pathname,response) {
+function route(handle,pathname,response, postData) {
 	console.log("Prestes a rotear uma requisição para " + pathname);
 	
 	if(typeof handle[pathname] === 'function') {
-		handle[pathname](response);
+		handle[pathname](response, postData);
 	} else {
 		console.log("Manipulador (handler) não encontrado para " + pathname);
 		response.writeHead(404, {"Content-Type": "text/plain"});
--- server.js
+++ server.js
@@ -6,10 +6,30 @@ var url = require("url"); // importando módulo url do node
 function start(route, handle) {
 	
 	function onRequest(request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Requisição para " + pathname +" recebida.");
+
+		// definindo que estou esperando dados com codificação UTF-8
+		//request.setEnconding("utf8");
+
+		/*	adicionando um event listener para a data que virá
+		* 	passo a	passo preenchendo a variável postData com
+		*	pedaços (chunk) que virão							*/
+		request.addListener("data", function postCatcher(postDataChunk) {
+			
+			postData += postDataChunk;
+			console.log("Recebido POST pedaço de dado '"+
+				postDataChunk + "'.");
+		});
+
+		/*	adicionando um event listener para que a chamada do route
+			seja feita apenas quando todos os dados do POST forem capturados */
+		request.addListener("end", function postTrown() {
+			/* passando o pepino pros handlers, inclusive os dados pegos */
+			route(handle, pathname, response, postData);
+		});		
 		
-		route(handle, pathname, response); // passando o pepino pros handlers
 	}
 	
 	rasAppService.createServer(onRequest).listen(8888);
